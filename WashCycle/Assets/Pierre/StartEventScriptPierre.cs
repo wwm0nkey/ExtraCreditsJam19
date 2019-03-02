6abd0596e0cf57716d7894252e05f60a0b9063cc
TheRealPieFace <34294484+TheRealPieFace@users.noreply.github.com>
2019-03-02 13:38:18 -0500

    Interaction

---
+++ WashCycle/Assets/Pierre/StartEventScriptPierre.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StartEventScriptPierre : MonoBehaviour
+{
+    public TMP_Text readyText;
+
+    public GameObject progressbar;
+    public bool isBroken = true;
+
+    Collider thisTrigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisTrigger = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckIfFixed();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            string text = "Hold@E";
+            readyText.text = text.Replace("@", System.Environment.NewLine);
+            progressbar.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            readyText.text = "";
+            progressbar.SetActive(false);
+        }
+    }
+    
+    public void FixMachine()
+    {
+        isBroken = false;
+    }
+
+    private void CheckIfFixed() { 
+    if (!isBroken) {
+            thisTrigger.enabled = false;
+            readyText.text = "";
+            progressbar.SetActive(false);
+        }
+    }
+
+}

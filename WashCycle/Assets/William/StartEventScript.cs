e3a95e2878ffca1e331ae4c2b45574a7c2d353f8
William Burke <wwm0nkey@outlook.com>
2019-03-02 00:51:00 -0500

    Added in WB video, set event trigger to press "E"

---
+++ WashCycle/Assets/William/StartEventScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StartEventScript : MonoBehaviour
+{
+    public TMP_Text readyText;
+
+    public GameObject progressbar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            readyText.text = "E";
+            progressbar.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            readyText.text = "";
+            progressbar.SetActive(false);
+        }
+    }
+    
+    
+}
